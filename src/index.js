4d9f964b08c3d36a92d28b1778cb88be7baddd1d
AT <mail@antontranelis.de>
2022-03-12 15:23:17 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//import leaflet_js from "../node_modules/leaflet/dist/leaflet.js";
+//import leaflet_extramarkers_js from "../node_modules/leaflet-extra-markers/dist/js/leaflet.extra-markers.js";
+//import leaflet_markercluster_js from "../node_modules/leaflet.markercluster/dist/leaflet.markercluster.js";
+//import leaflet_markercluster_layersupport_js from "../node_modules/leaflet.markercluster.layersupport/dist/leaflet.markercluster.layersupport.js";
+//import leaflet_photon_js from "../node_modules/leaflet.photon/leaflet.photon.js";
+//import leaflet_locatecontrol_js from  "../node_modules/leaflet.locatecontrol/dist/L.Control.Locate.min.js";
+
+/*
+import mapStyle from "../public/css/map.css";
+import utopiaStyle from "../public/css/utopia.css";
+
+import leaflet_css from "../node_modules/leaflet/dist/leaflet.css";
+import leaflet_extramarkers_css from "../node_modules/leaflet-extra-markers/dist/css/leaflet.extra-markers.min.css";
+import leaflet_markercluster_css from "../node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css";
+// layersupport has no css
+import leaflet_photon_css from "../node_modules/leaflet.photon/leaflet.photon.css";
+import leaflet_locatecontrol_css from  "../node_modules/leaflet.locatecontrol/dist/L.Control.Locate.min.css";
+
+*/
+
+
+//import fontawesome from "../static/vendors/fontawesome/css/all.css";
